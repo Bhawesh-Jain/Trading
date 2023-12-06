397ad51a9eea23d142bdf403f7582cf19657188b
Bhawesh <bhaweshjainskype@gmail.com>
2023-12-06 17:45:15 +0530

    Final

--- app/src/main/java/com/abmtech/trading/ui/ComplianceActivity.java
+++ app/src/main/java/com/abmtech/trading/ui/ComplianceActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.abmtech.trading.R;
+import com.abmtech.trading.databinding.ActivityComplianceBinding;
 
 public class ComplianceActivity extends AppCompatActivity {
-
+    private ActivityComplianceBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compliance);
+        binding = ActivityComplianceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        findViewById(R.id.image_back).setOnClickListener(v -> onBackPressed());
+
+        binding.icBack.setOnClickListener(v -> onBackPressed());
     }
 }
